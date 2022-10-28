2db2ac162f69bfcb5b888cfcc6e9611deb967cf0
Cirũ Franklin <cirufrank@me.com>
2022-10-28 12:09:45 -0700

    Complete unti tests for the service layer

--- VendingMachine/src/test/java/com/we/vendingmachine/service/VendingMachineServiceLayerImplTest.java
+++ VendingMachine/src/test/java/com/we/vendingmachine/service/VendingMachineServiceLayerImplTest.java
@@ -27,8 +27,14 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  *
- * @author ciruf
+ * @author Cirũ Franklin (she/they), Software Engineer
+ * @course DI002 Full Stack Development Using Java and React (2210)
+ * @project Assessment: Vending Machine
+ * 
+ * @description This class automates the Unit tests for the 
+ * VendingMachineServiceLayer using JUnit
  */
+
 @ExtendWith(VendingMachineServiceImplParameterResolver.class)
 public class VendingMachineServiceLayerImplTest {
     
@@ -39,25 +45,53 @@ public class VendingMachineServiceLayerImplTest {
     @ValueSource(ints = {3})
     public void testPurchaseItem(int itemId, VendingMachineServiceLayerImpl testServiceLayer) throws VendingMachineInsufficientFundsException,
             VendingMachineDaoPersistenceException, VendingMachineItemUnavailableException {
+        final CoinName QUARTER = CoinName.QUARTER;
         final VendingMachineDaoStubFileImpl publicTestDao = new VendingMachineDaoStubFileImpl("test-inventory.txt");
         final VendingMachineBankDaoStubFileImpl publicTestBankDao = new VendingMachineBankDaoStubFileImpl("test-coin-inventory.txt");
         final BigDecimal testChange = new BigDecimal("2.00");
         final VendingMachineItem testItem = publicTestDao.getItem(itemId);
         final int previousTotalInStock = testItem.getNumOfItems();
-        final int previousQuartersInStock = publicTestBankDao.getCoin(CoinName.QUARTER).getCoinTotal();
-        final int accurateChangeNum = 6, ONE_ITEM = 1;
-        final CoinName QUARTER = CoinName.QUARTER;
+        final int previousQuartersInStock = publicTestBankDao.getCoin(QUARTER).getCoinTotal();
+        final int ACCURATE_QUARTER_CHANGE_TOTAL = 6, ONE_ITEM = 1;
         UserChange totalChange = testServiceLayer.purchaseItem(testChange, testItem);
-        final int currentQuartersInStock = publicTestBankDao.getCoin(CoinName.QUARTER).getCoinTotal();
+        final int currentQuartersInStock = publicTestBankDao.getCoin(QUARTER).getCoinTotal();
         Coin userQuarters = totalChange.getQuarters();
         ArrayList<Coin> userCoins = totalChange.getCoins();
         for (Coin currentCoin : userCoins) {
             System.out.println(currentCoin.toString());
         }
         final VendingMachineItem updatedTestItem = publicTestDao.getItem(itemId);
-        assertEquals(accurateChangeNum, userQuarters.getCoinTotal());
+        assertEquals(ACCURATE_QUARTER_CHANGE_TOTAL, userQuarters.getCoinTotal());
         assertEquals(previousTotalInStock, updatedTestItem.getNumOfItems() + ONE_ITEM);
-        assertEquals(previousQuartersInStock, currentQuartersInStock + accurateChangeNum);
+        assertEquals(previousQuartersInStock, currentQuartersInStock + ACCURATE_QUARTER_CHANGE_TOTAL);
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {9})
+    public void testPurchaseItemOutOfStock(int itemId, VendingMachineServiceLayerImpl testServiceLayer) throws VendingMachineInsufficientFundsException,
+            VendingMachineDaoPersistenceException {
+        final CoinName QUARTER = CoinName.QUARTER;
+        final VendingMachineDaoStubFileImpl publicTestDao = new VendingMachineDaoStubFileImpl("test-inventory.txt");
+        final BigDecimal testChange = new BigDecimal("2.00");
+        final VendingMachineItem testItem = publicTestDao.getItem(itemId);
+        final int ACCURATE_QUARTER_CHANGE_TOTAL = 6, ONE_ITEM = 1;
+        assertThrows(VendingMachineItemUnavailableException.class, () -> {
+            UserChange totalChange = testServiceLayer.purchaseItem(testChange, testItem);
+        });
+        
+    }
+    @ParameterizedTest
+    @ValueSource(ints = {9})
+    public void testAttemptInsufficientFundPurchase(int itemId, VendingMachineServiceLayerImpl testServiceLayer) throws
+            VendingMachineDaoPersistenceException, VendingMachineItemUnavailableException {
+        final CoinName QUARTER = CoinName.QUARTER;
+        final VendingMachineDaoStubFileImpl publicTestDao = new VendingMachineDaoStubFileImpl("test-inventory.txt");
+        final BigDecimal testChange = new BigDecimal("0.00");
+        final VendingMachineItem testItem = publicTestDao.getItem(itemId);
+        final int ACCURATE_QUARTER_CHANGE_TOTAL = 6, ONE_ITEM = 1;
+        assertThrows(VendingMachineInsufficientFundsException.class, () -> {
+            UserChange totalChange = testServiceLayer.purchaseItem(testChange, testItem);
+        });
+        
     }
     
     
